60dcce851de4ccf4807bd263aca00239c8b3bcd5
DennisMuia <dennismuia.dm@gmail.com>
2022-09-12 04:47:24 -0700

    5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always (0)
+ *
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch = 'z' ; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
