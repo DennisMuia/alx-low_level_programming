5c31d659b07b0f43ef034b85d611cb99d393634e
DennisMuia <dennismuia.dm@gmail.com>
2022-09-12 04:18:23 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
