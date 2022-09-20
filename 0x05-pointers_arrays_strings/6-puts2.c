63a23f90636c2ab007371656a989dcfbb83e4184
DennisMuia <dennismuia.dm@gmail.com>
2022-09-20 03:30:45 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character from first followed by new line
+ * @str: string
+ *
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + 1) != '\0')
+	{
+		if (i % 2 == 0)
+			putchar (*(str + i));
+		i++;
+	}
+	putchar(10);
+}
