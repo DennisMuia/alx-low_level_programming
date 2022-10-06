0a4b8e7074a2fd267a25c7781e13ea75a1d1ece2
DennisMuia <dennismuia.dm@gmail.com>
2022-10-06 14:37:48 -0700

    More Malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,22 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * _strlen - return string length
+ * @string: string
+ *
+ * Return: string length
+ */
+
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++);
+
+	return (i);
+}
 
 /**
  * string_nconcat - concatenates 2 strings.
